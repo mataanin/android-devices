332c85012efc1aafdb52cc218b7cc687c9d3e2bf
Antony Koch <ant@iamkoch.com>
2013-10-25 15:54:51 +0100

    rubby script to search models

---
+++ search-models.rb
@@ -0,0 +1,17 @@
+if __FILE__ == $0
+  answers = []
+  ARGV.each do|a|
+    puts "Matching #{a}"
+    
+    file = File.open('Build.Model - models.csv').read
+    file.gsub!(/\r\n?/, "\n")
+    file.each_line do |line|
+      spl = line.split(',')
+      if spl[1].include?(a)
+        answers << "SUPPORTED_DEVICES.add(\"#{spl[0]}\"); // #{spl[1]} - #{spl[3]}"
+      end
+    end
+    
+  end
+  puts answers
+end
